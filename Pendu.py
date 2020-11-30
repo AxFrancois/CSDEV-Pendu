efbd9ea468c19c5dddcb1c3d9c3ccfede4ba7f08
Axel <axel.francois-charlot@cpe.fr>
2020-11-30 14:42:24 +0100

    "programme tri + liste triée"

--- Pendu.py
+++ Pendu.py
@@ -5,6 +5,7 @@ Created on Sun Nov 29 12:10:36 2020
 @author: Axel François
 """
 import csv
+import random
 
 def lectureDonnee(doc):
     valren = []
@@ -14,6 +15,41 @@ def lectureDonnee(doc):
             valren.append(', '.join(row))
     return valren
 
-Test = lectureDonnee('Mot_Pendu.csv')
-print(Test)
-print(isinstance(Test[0],str))
+ListeMot = lectureDonnee('Mot_Pendu.csv')
+ListeLettre = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+
+Play = True
+
+
+def fTour(pMot, pLettreEntrée, pDisplay, pListeLettreDonnée):
+    pMot = list(pMot)
+    pDisplay = list(pDisplay)
+    for i in range(len(pMot)):
+        if pMot[i] == pLettreEntrée:
+            pDisplay[i] = pLettreEntrée
+    pListeLettreDonnée.append(pLettreEntrée)
+    pDisplay = ''.join(pDisplay)    
+    return  pDisplay, pListeLettreDonnée
+
+
+
+    
+
+while Play == True:
+    Mot = ListeMot[random.randint(0,len(ListeMot)-1)]
+    ListeLettreEntrée = []
+    Display = "_"*len(Mot)
+    
+    Display,ListeLettreEntrée = fTour(Mot, Mot[0], Display, ListeLettreEntrée)
+    Display,ListeLettreEntrée = fTour(Mot, Mot[-1], Display, ListeLettreEntrée)
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    Play = False
